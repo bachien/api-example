109204ec8f5a968743468cd34a0b7f84292ea7f9
Ba-Chen <ba-chen.phi@keyconsulting.fr>
2020-09-21 18:29:21 +0700

    update count function

--- routes/countRoutes.js
+++ routes/countRoutes.js
@@ -1,24 +1,57 @@
 module.exports = app => {
   app.get('/api/count', (req, res) => {
     console.log("api run");
-    res.send('Hello');
-  });
-
-  app.get('/api/get-new-survey', (req, res) => {
     res.send({
-      body: '',
-      from: '',
-      subject: '',
-      title: '',
-      recipients: ''
+      count: 0
     });
   });
 
-  app.post("/api/count/plus", async (req, res) => {
+  app.post("/api/count/plus1", async (req, res) => {
     const { count } = req.body;
     console.log(count)
     res.send({
       count: count + 1
+    });
+  });
+
+  app.post("/api/count/plus10", async (req, res) => {
+    const { count } = req.body;
+    console.log(count)
+    res.send({
+      count: count + 10
+    });
+  });
+
+  app.post("/api/count/minus1", async (req, res) => {
+    const { count } = req.body;
+    console.log(count)
+    res.send({
+      count: count - 1
+    });
+  });
+
+  app.post("/api/count/minus10", async (req, res) => {
+    const { count } = req.body;
+    console.log(count)
+    res.send({
+      count: count - 10
+    });
+  });
+
+
+  app.post("/api/count/x2", async (req, res) => {
+    const { count } = req.body;
+    console.log(count)
+    res.send({
+      count: count * 2
+    });
+  });
+
+  app.post("/api/count/x10", async (req, res) => {
+    const { count } = req.body;
+    console.log(count)
+    res.send({
+      count: count * 10
     });
   });
 
